c9a6a09cf6a44e31aead5c03624a5f44f155438c
“Pradeepta <selenautoqa@gmail.com>
2024-09-21 16:40:30 +0530

    3rd commit

--- src/test/java/grid/TestRemote.java
+++ src/test/java/grid/TestRemote.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -15,9 +16,12 @@ public class TestRemote {
 	public void googleTest() throws MalformedURLException{
 
 
-		FirefoxOptions opt = new FirefoxOptions();
+		//FirefoxOptions opt = new FirefoxOptions();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+
 		
-		WebDriver driver = new RemoteWebDriver(new URL("http://13.126.27.58/"),opt);
+		WebDriver driver = new RemoteWebDriver(new URL("http://13.201.88.57:4444"),capabilities);
 		driver.get("http://google.com");
 		driver.findElement(By.name("q")).sendKeys("Hello Grid !!!");
 		System.out.println(driver.getTitle());
